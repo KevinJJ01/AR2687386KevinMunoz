b55732f5e5a3d90bb90c44337e7e050ab04d4c6d
Juanpa0515 <jpsuarez314@misena.edu.co>
2023-09-11 21:34:44 -0500

    Initial commit

---
+++ coedusenaapp2687340_johan_yesid_munoz_castaneda/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "co.edu.sena.app2687340_johan_yesid_munoz_castaneda"
+include(":app")
